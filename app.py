400b751af534fa3686fef831f11b49a3fc450ba7
Rawann0m <raawannm@gmail.com>
2026-02-22 23:04:17 +0300

    Add gitignore to exclude cache files

--- app.py
+++ app.py
@@ -1,28 +1,6 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+def add(a, b):
+    return a + b
 
-PORT = 8000
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-
-        content = """
-<html>
-<head>
-    <title>DevSecOps Lab</title>
-</head>
-<body style="background-color:#111; color:white; text-align:center; font-family:Arial;">
-    <h1>DevSecOps Lab </h1>
-    <p>Running Successfully.</p>
-    <p>Local Development Environment Working.</p>
-</body>
-</html>
-"""
-
-        self.wfile.write(content.encode())
-
-server = HTTPServer(("", PORT), MyHandler)
-print(f"Server running on http://localhost:{PORT}")
-server.serve_forever()
+if __name__ == "__main__":
+    print("DevSecOps Lab Running 🚀")
